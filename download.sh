e549c8b0dc9f05e3a60db38d09086ab164203e8e
wlc952 <wlc@zju.edu.cn>
2024-11-14 14:09:39 +0800

    pcie

--- download.sh
+++ download.sh
@@ -7,9 +7,9 @@ cd bmodels
 # 下载模型
 # python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/phi3-4b_int4_1dev.bmodel
 
-# python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/qwen2.5-7b_int4_seq8192_1dev.bmodel
+python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/qwen2.5-3b_int4_seq512_1dev.bmodel
 
-python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/qwen1.5-1.8b_int8_1dev_seq1280.bmodel
+# python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/qwen1.5-1.8b_int8_1dev_seq1280.bmodel
 
 cd ..
 
